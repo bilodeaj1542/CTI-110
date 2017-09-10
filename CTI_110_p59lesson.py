14261e71991009cdc8963ba6624502d1d0c74766
bilodeaj1542 <31809605+bilodeaj1542@users.noreply.github.com>
2017-09-10 19:19:08 -0400

    Add files via upload

---
+++ CTI_110_p59lesson.py
@@ -0,0 +1,10 @@
+# CTI-110
+# Calculating an average p.59
+# Jason Bilodeau
+# September 10, 2017
+
+test1 = float(input("Enter your score from test 1: "))
+test2 = float(input("Enter your score from test 2: "))
+test3 = float(input("Enter your score from test 3: "))
+average = (test1 + test2 + test3) / 3.0
+print ("Your average test score is ", average) 
