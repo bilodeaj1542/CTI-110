a79faeac639ee08dff0c458a1921624364101be2
bilodeaj1542 <31809605+bilodeaj1542@users.noreply.github.com>
2017-09-10 17:01:26 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_JasonBilodeau.py
@@ -0,0 +1,16 @@
+# CTI-110
+# M2HW1-Distance Traveled
+# Jason Bilodeau
+# September 10, 2017
+# Program to determine distance traveled 
+
+speed = 70
+disaft6 = speed * 6
+disaft10 = speed * 10
+distaft15 = speed * 15
+
+print (disaft6) # Distance travelled after 6 hours at 70 mph
+print (disaft10) # Distance traveled after 10 hours at 70 mph
+print (distaft15) # Distance traveled after 15 hours at 70 mph 
+
+
