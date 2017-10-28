0df09d64659c652bebc821a7448fbdd0643eaa59
bilodeaj1542 <31809605+bilodeaj1542@users.noreply.github.com>
2017-10-28 14:34:16 -0400

    Add files via upload

---
+++ M5HW3_bilodeau.py
@@ -0,0 +1,13 @@
+#CTI 110
+# M5HW3: Factorial
+# October 28, 2017
+# Jason Bilodeau
+
+
+number = int(input("Enter a non negative interger: "))
+total = 1
+for a in range(number):
+    total = total * (a+1)
+
+print ("Total is:", total) 
+             
